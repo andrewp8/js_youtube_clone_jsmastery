6fb12ce01ea89a6184f7204cc078426d2821f1c8
Andrew Pham <minhanh.vietpham@gmail.com>
2023-05-08 18:39:19 -0500

    PC - successed call Youtube v3 API

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const axios = require('axios');
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search';
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-  url: BASE_URL,
   params: {
     maxResults: '50'
   },
@@ -13,3 +11,8 @@ const options = {
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+}
