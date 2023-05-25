fffa8621d55a54ded5912c0b5b33fbae6d300275
Andrew Pham <minhanh.vietpham@gmail.com>
2023-05-24 22:44:19 -0500

    PC - 1:51:30 -Working on the SearchFeed, working on how to pass input from param to using on search qeury API

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos } from "./";
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+	const [videos, setVideos] = useState([]);
 
-export default SearchFeed
+	useEffect(() => {
+		fetchFromAPI(`search?part=snippet&q=`).then((data) => {
+			setVideos(data.items);
+		});
+	}, []);
+
+	return (
+		// height: "90vh"
+		<Box p={2} sx={{ overFlowY: "auto", height: "100%", flex: 2 }}>
+			<Typography variant="h4" fontWeight="bold" md={2} sx={{ color: "white" }}>
+				Search Results for: <span style={{ color: "#F31503" }}>videos</span>
+			</Typography>
+			<Videos videos={videos} />
+		</Box>
+	);
+};
+
+export default SearchFeed;
