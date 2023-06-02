b6aac69fbf6849d493930992c7771ad044456e53
Andrew Pham <minhanh.vietpham@gmail.com>
2023-06-01 22:50:19 -0500

    PC - implemented <SearchBar/> (add handleSubmit() to onChange(). Also, get the input value and use it as the  to navigate to the /search/)

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
 
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { Videos } from "./";
 
 const SearchFeed = () => {
 	const [videos, setVideos] = useState([]);
+	const { searchTerm } = useParams();
 
 	useEffect(() => {
-		fetchFromAPI(`search?part=snippet&q=`).then((data) => {
+		fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
 			setVideos(data.items);
 		});
-	}, []);
+	}, [searchTerm]);
 
 	return (
 		// height: "90vh"
 		<Box p={2} sx={{ overFlowY: "auto", height: "100%", flex: 2 }}>
 			<Typography variant="h4" fontWeight="bold" md={2} sx={{ color: "white" }}>
-				Search Results for: <span style={{ color: "#F31503" }}>videos</span>
+				Search Results for:{" "}
+				<span style={{ color: "#F31503" }}>{searchTerm}</span> videos
 			</Typography>
 			<Videos videos={videos} />
 		</Box>
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+
+import { Video } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
-  return (
-    <div>VideoDetail</div>
-  )
-}
+	return <div>VideoDetail</div>;
+};
 
-export default VideoDetail
+export default VideoDetail;
